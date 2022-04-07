6d0450901f82e0110c8618bb6acf91d6dacf6b44
Nathan <nschpro@gmail.com>
2022-04-07 15:12:35 +0200

    Big Update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const finalColumn = document.querySelectorAll('[data-final-column]')
-const yourScore = document.querySelectorAll('[data-your-score')
-const computerScore = document.querySelectorAll('[data-computer-score')
-const SELECTIONS = [
-    {
-        name: 'pierre',
-        emoji: '✊',
-        beats: 'ciseau'
-    },
-    {
-        name: 'feuille',
-        emoji: '✋',
-        beats: 'pierre'
-    },
-    {
-        name: 'ciseau',
-        emoji: '✌️',
-        beats: 'feuille'
-    }
-]
+const SELECTIONS = ['pierre','feuille','ciseau'];
 
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-      const selectionName = selectionButton.dataset.selection
-      const selection = SELECTIONS.find(selection => selection.name === selectionName)
-      makeSelection(selection)
+function randomz(){
+    let out = "";
+    out = Math.floor(Math.random() * 3);
+    return out;
+}
+
+function jeu(joueur, ordi){
+    if(joueur === ordi){
+        return "e";
+    }
+    if(joueur === 0){
+        if(ordi === 2){
+            return "v";
+        }
+        else{
+            return "d";
+        }
+    }
+    if(joueur === 1){
+        if (ordi === 0){
+            return "v";
+        }
+        else{
+            return "d";
+        }
+    }
+    if(joueur === 2){
+        if(ordi === 1){
+            return "v";
+        }
+        else{
+            return "d";
+        }
+    }
+}
+
+let win = document.getElementById("vic");
+let loss = document.getElementById("def");
+let egal = document.getElementById("nul");
+
+let ls_selection = document.querySelectorAll(".selectionj")
+
+ls_selection.forEach((element,keys) => {
+    element.addEventListener('click', ()=>{
+        console.log(jeu(keys,randomz()))
+        save(jeu(keys,randomz()))
     })
-  })
-  
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS[randomIndex]
-  }
+});
+
+function save(result){
+    if(result === 'v'){
+        win.textContent = Number(win.textContent)+1;
+    }
+    else if(result === 'd'){
+        loss.textContent = Number(win.textContent)+1;
+    }
+    else{
+        egal.textContent = Number(win.textContent)+1;
+    }
+}
